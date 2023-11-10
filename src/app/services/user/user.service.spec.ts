cacde506a81a1625e08d3e84de612d5f68a1510f
Ayse Omercik <ayse.omercik@outlook.com>
2023-11-10 18:07:15 +0100

    added test case for user service component

--- src/app/services/user/user.service.spec.ts
+++ src/app/services/user/user.service.spec.ts
@@ -1,29 +1,53 @@
+import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { getTestBed, TestBed } from '@angular/core/testing';
 import { UserService } from './user.service';
 import { HttpApiService } from '../http-api';
+import { User } from 'src/app/models/user';
 
 describe('UserService', () => {
-    let injector: TestBed;
-    let service: UserService;
-    let httpMock: HttpTestingController;
+    let userService: UserService;
+    let httpTestingController: HttpTestingController;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule],
-            providers: [UserService, HttpApiService]
-        }).compileComponents();
+            providers: [UserService, HttpApiService],
+        });
 
-        injector = getTestBed();
-        service = injector.get(UserService);
-        httpMock = injector.get(HttpTestingController);
+        userService = TestBed.inject(UserService);
+        httpTestingController = TestBed.inject(HttpTestingController);
     });
 
     afterEach(() => {
-        httpMock.verify();
+        httpTestingController.verify();
     });
 
-    it('should be created', () => {
-        expect(service).toBeTruthy();
-    });
+    it('should fetch user details', inject(
+        [HttpTestingController],
+        (httpClient: HttpTestingController) => {
+            const userId = 1;
+            const userResponse = {
+                data: {
+                    id: 1,
+                    first_name: 'John',
+                    last_name: 'Doe',
+                    email: 'john.doe@example.com',
+                    avatar: 'path/to/avatar.jpg',
+                    isFavourite: false,
+                    isDisabled: false,
+                }
+            };
+
+            userService.getUsers(userId).subscribe((user: User) => {
+                expect(user.id).toEqual(userId);
+                expect(user.first_name).toEqual('John');
+                expect(user.last_name).toEqual('Doe');
+                expect(user.email).toEqual('john.doe@example.com');
+            });
+
+            const req = httpTestingController.expectOne(`https://reqres.in/api/users/${userId}`);
+            expect(req.request.method).toEqual('GET');
+            req.flush(userResponse);
+        }
+    ));
 });
