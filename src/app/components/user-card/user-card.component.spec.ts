387a854ccf725cfceac499fdd736426529f0e117
Ayse Omercik <ayse.omercik@outlook.com>
2023-11-10 17:40:09 +0100

    added test case for user card component

--- src/app/components/user-card/user-card.component.spec.ts
+++ src/app/components/user-card/user-card.component.spec.ts
@@ -1,16 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserCardComponent } from './user-card.component';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('UserCardComponent', () => {
   let component: UserCardComponent;
   let fixture: ComponentFixture<UserCardComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [UserCardComponent],
-      imports: [MatCardModule]
-    });
+      imports: [MatCardModule, MatIconModule, BrowserAnimationsModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +23,77 @@ describe('UserCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit selectedUser when toggleFavourite is called', () => {
+    const user = {
+      id: 1,
+      first_name: 'John',
+      last_name: 'Doe',
+      email: 'john.doe@example.com',
+      avatar: 'path/to/avatar.jpg',
+      isFavourite: false,
+      isDisabled: false,
+    };
+
+    spyOn(component.selectedUser, 'emit');
+
+    component.user = user;
+    component.toggleFavourite();
+
+    expect(component.user.isFavourite).toBe(true);
+    expect(component.selectedUser.emit).toHaveBeenCalledWith(user);
+  });
+
+  it('should display user information in the template', () => {
+    const user = {
+      id: 1,
+      first_name: 'John',
+      last_name: 'Doe',
+      email: 'john.doe@example.com',
+      avatar: 'path/to/avatar.jpg',
+      isFavourite: false,
+      isDisabled: false,
+    };
+
+    component.user = user;
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+    console.log({ compiled })
+    
+    const titleElement = compiled.querySelector('.user-card-title');
+    const subtitleElement = compiled.querySelector('.user-card-subtitle');
+    const imageElement = compiled.querySelector('.user-card-image');
+
+    console.log({ titleElement })
+    expect(titleElement).toBeTruthy();
+    expect(titleElement.textContent).toContain('John Doe');
+
+    expect(subtitleElement).toBeTruthy();
+    expect(subtitleElement.textContent).toContain('john.doe@example.com');
+
+    expect(imageElement).toBeTruthy();
+    expect(imageElement.getAttribute('src')).toBe('path/to/avatar.jpg');
+  });
+
+  it('should disable the button if user.isDisabled is true', () => {
+    const user = {
+      id: 1,
+      first_name: 'John',
+      last_name: 'Doe',
+      email: 'john.doe@example.com',
+      avatar: 'path/to/avatar.jpg',
+      isFavourite: false,
+      isDisabled: true,
+    };
+
+    component.user = user;
+    fixture.detectChanges();
+
+    const compiled = fixture.debugElement.nativeElement;
+    const button = compiled.querySelector('.user-card-actions button[disabled]');
+
+    expect(button).toBeTruthy();
   });
 });
