819b1c0c129768f681bf70ca261da2f90ec57e81
Ayse Omercik <ayse.omercik@outlook.com>
2023-11-10 18:00:37 +0100

    added test case for http api service

--- src/app/services/http-api/http-api.service.spec.ts
+++ src/app/services/http-api/http-api.service.spec.ts
@@ -1,28 +1,97 @@
+import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
-import { getTestBed, TestBed } from '@angular/core/testing';
 import { HttpApiService } from './http-api.service';
 
 describe('HttpApiService', () => {
-    let injector: TestBed;
-    let service: HttpApiService;
-    let httpMock: HttpTestingController;
+    let httpApiService: HttpApiService;
+    let httpTestingController: HttpTestingController;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule],
-            providers: [HttpApiService]
-        }).compileComponents();
+            providers: [HttpApiService],
+        });
 
-        injector = getTestBed();
-        service = injector.get(HttpApiService);
-        httpMock = injector.get(HttpTestingController);
+        httpApiService = TestBed.inject(HttpApiService);
+        httpTestingController = TestBed.inject(HttpTestingController);
     });
 
     afterEach(() => {
-        httpMock.verify();
+        httpTestingController.verify();
     });
 
     it('should be created', () => {
-        expect(service).toBeTruthy();
+        expect(httpApiService).toBeTruthy();
     });
+
+    it('should send a GET request', inject(
+        [HttpTestingController], (httpClient: HttpTestingController) => {
+            const apiUrl = 'https://example.com';
+            const path = '/sample';
+            const response = { data: 'sample data' };
+
+            httpApiService.get(apiUrl, path).subscribe((data) => {
+                expect(data).toEqual(response);
+            });
+
+            const req = httpTestingController.expectOne(`${apiUrl}/api${path}`);
+            expect(req.request.method).toEqual('GET');
+            req.flush(response);
+        }
+    ));
+
+    it('should send a POST request', inject(
+        [HttpTestingController],
+        (httpClient: HttpTestingController) => {
+            const apiUrl = 'https://example.com';
+            const path = '/sample';
+            const requestBody = { key: 'value' };
+            const response = { data: 'sample data' };
+
+            httpApiService.post(apiUrl, path, requestBody).subscribe((data) => {
+                expect(data).toEqual(response);
+            });
+
+            const req = httpTestingController.expectOne(`${apiUrl}/api${path}`);
+            expect(req.request.method).toEqual('POST');
+            expect(req.request.body).toEqual(requestBody);
+            req.flush(response);
+        }
+    ));
+
+    it('should send a PUT request', inject(
+        [HttpTestingController],
+        (httpClient: HttpTestingController) => {
+            const apiUrl = 'https://example.com';
+            const path = '/sample';
+            const requestBody = { key: 'value' };
+            const response = { data: 'sample data' };
+
+            httpApiService.put(apiUrl, path, requestBody).subscribe((data) => {
+                expect(data).toEqual(response);
+            });
+
+            const req = httpTestingController.expectOne(`${apiUrl}/api${path}`);
+            expect(req.request.method).toEqual('PUT');
+            expect(req.request.body).toEqual(requestBody);
+            req.flush(response);
+        }
+    ));
+
+    it('should send a DELETE request', inject(
+        [HttpTestingController],
+        (httpClient: HttpTestingController) => {
+            const apiUrl = 'https://example.com';
+            const path = '/sample';
+            const response = { data: 'sample data' };
+
+            httpApiService.delete(apiUrl, path).subscribe((data) => {
+                expect(data).toEqual(response);
+            });
+
+            const req = httpTestingController.expectOne(`${apiUrl}/api${path}`);
+            expect(req.request.method).toEqual('DELETE');
+            req.flush(response);
+        }
+    ));
 });
